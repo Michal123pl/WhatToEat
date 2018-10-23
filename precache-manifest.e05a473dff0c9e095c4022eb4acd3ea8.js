b37617ca9682b02d3b0831a57c7b37fe5815449b
Michal <michalnow777@interia.pl>
2018-10-23 17:03:01 +0200

    Updates

---
+++ precache-manifest.e05a473dff0c9e095c4022eb4acd3ea8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8b85bd0c5c6cd384b3",
+    "url": "/Eat/static/js/runtime~main.7b8b85bd.js"
+  },
+  {
+    "revision": "039acde5e247325b60d1",
+    "url": "/Eat/static/js/main.039acde5.chunk.js"
+  },
+  {
+    "revision": "17b085f3881ce991afc8",
+    "url": "/Eat/static/js/1.17b085f3.chunk.js"
+  },
+  {
+    "revision": "039acde5e247325b60d1",
+    "url": "/Eat/static/css/main.a0486153.chunk.css"
+  },
+  {
+    "revision": "8bb05947cb59d025d6d1f605dbb9b078",
+    "url": "/Eat/index.html"
+  }
+];
